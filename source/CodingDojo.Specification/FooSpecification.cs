8afab6c174525485b53efb046571016c9a753af7
Urs Enzler <urs.enzler@gmail.com>
2012-11-27 16:09:21 +0100

    added coding dojo project skeleton

---
+++ source/CodingDojo.Specification/FooSpecification.cs
@@ -0,0 +1,12 @@
+﻿namespace CodingDojo.Specification
+{
+    using FluentAssertions;
+
+    using Machine.Specifications;
+
+    [Subject("Foo")]
+    public class When_doing_foo
+    {
+        It should_be_fooed = () => "foo".Should().NotBe("bar");
+    }
+}
---
+++ source/CodingDojo/Foo.cs
@@ -0,0 +1,6 @@
+﻿namespace CodingDojo
+{
+    public class Foo
+    {
+    }
+}
